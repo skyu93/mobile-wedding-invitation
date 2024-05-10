c8f65bb2151b619e9fdf9939ff70764246fc6f48
Teo <teo@clobot.co.kr>
2024-05-10 10:06:03 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="warp">
+    <header></header>
+    <main></main>
+    <footer></footer>
+  </div>
+</template>
+
+<style scoped>
+main {
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  background: #ffc8ae;
+}
+@media screen and (min-width: 674px) {
+  main {
+    width: 1136px;
+  }
+}
+</style>
